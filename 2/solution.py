b7f5343cd736231d83c6dcdee50d23e2b312d767
b00d1e <0xb00d1e@protonmail.com>
2020-12-26 14:07:07 -0500

    Day 2 - Part 2

--- 2/solution.py
+++ 2/solution.py
@@ -3,12 +3,12 @@ import re
 
 def part1():
     data = set(load_data())
-    policies = parse_policies(data)
-    valid_policies = find_valid_policies(policies)
+    policies = parse_policies_1(data)
+    valid_policies = find_valid_policies_1(policies)
     print(f'Answer - Part 1: {len(valid_policies)}')
 
 
-def find_valid_policies(policies):
+def find_valid_policies_1(policies):
     valid_policies = []
     for policy in policies:
         character_count = policy['password'].count(policy['character'])
@@ -17,7 +17,7 @@ def find_valid_policies(policies):
     return valid_policies
 
 
-def parse_policies(data):
+def parse_policies_1(data):
     policies = []
     pattern = re.compile(r'(?P<min_occurrences>\d+)-(?P<max_occurrences>\d+)\s(?P<character>\w):\s(?P<password>.*)')
     for line in data:
@@ -25,6 +25,39 @@ def parse_policies(data):
         matches = re.match(pattern, line)
         policy['min_occurrences'] = int(matches.group('min_occurrences'))
         policy['max_occurrences'] = int(matches.group('max_occurrences'))
+        policy['character'] = matches.group('character')
+        policy['password'] = matches.group('password')
+        policies.append(policy)
+    return policies
+
+
+def part2():
+    data = set(load_data())
+    policies = parse_policies_2(data)
+    valid_policies = find_valid_policies_2(policies)
+    print(f'Answer - Part 2: {len(valid_policies)}')
+
+
+def find_valid_policies_2(policies):
+    valid_policies = []
+    for policy in policies:
+        character = policy['character']
+        password = policy['password']
+        position_1 = policy['position_1']
+        position_2 = policy['position_2']
+        if (password[position_1] == character) ^ (password[position_2] == character):
+            valid_policies.append(policy)
+    return valid_policies
+
+
+def parse_policies_2(data):
+    policies = []
+    pattern = re.compile(r'(?P<position_1>\d+)-(?P<position_2>\d+)\s(?P<character>\w):\s(?P<password>.*)')
+    for line in data:
+        policy = {}
+        matches = re.match(pattern, line)
+        policy['position_1'] = int(matches.group('position_1')) - 1
+        policy['position_2'] = int(matches.group('position_2')) - 1
         policy['character'] = matches.group('character')
         policy['password'] = matches.group('password')
         policies.append(policy)
@@ -39,3 +72,4 @@ def load_data():
 
 if __name__ == '__main__':
     part1()
+    part2()
