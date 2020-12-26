7fd71d5d5b9628067f375e675faeee6c36b805e4
b00d1e <0xb00d1e@protonmail.com>
2020-12-26 14:44:51 -0500

    Day 3 - Part 2

--- 3/solution.py
+++ 3/solution.py
@@ -1,7 +1,9 @@
+import functools
+
+
 def part1():
     data = load_data()
     grid = parse_data(data)
-    step = (3, 1)
     i = 1
     tree_count = 0
     width = len(data[0])
@@ -14,6 +16,29 @@ def part1():
             break
         i += 1
     print(f'Answer - Part 1: {tree_count}')
+
+
+def part2():
+    data = load_data()
+    grid = parse_data(data)
+    steps = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    tree_counts = []
+    width = len(data[0])
+
+    for step in steps:
+        tree_count = 0
+        i = 1
+        while True:
+            try:
+                if grid[((step[0]*i) % width, step[1]*i)] == '#':
+                    tree_count += 1
+            except KeyError:
+                break
+            i += 1
+        tree_counts.append(tree_count)
+    
+    product = functools.reduce(lambda x,y: x*y, tree_counts)
+    print(f'Answer - Part 2: {product}')
 
 
 def parse_data(data):
@@ -32,3 +57,4 @@ def load_data():
 
 if __name__ == '__main__':
     part1()
+    part2()
