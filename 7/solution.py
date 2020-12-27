739c09f3205e5fc2f83eb5a04cf398e1c2427d0a
b00d1e <0xb00d1e@protonmail.com>
2020-12-27 16:30:31 -0500

    Day 7 - Part 2

--- 7/solution.py
+++ 7/solution.py
@@ -12,18 +12,36 @@ def part1():
     print(f'Answer - Part 1: {count}')
 
 
+def part2():
+    input_data = load_data()
+    rules = parse_rules(input_data)
+    count = count_of_inner_bags(rules, 'shiny gold')
+    print(f'Answer - Part 2: {count}')
+
+
 def count_of_gold(bag, rules):
     count = 0
     inner_bags = rules[bag]
     if not inner_bags:
         return count
-    else:
-        for inner_bag in inner_bags:
-            if inner_bag['bag_color'] == 'shiny gold':
-                count += 1
-            count += count_of_gold(inner_bag['bag_color'], rules)
+    for inner_bag in inner_bags:
+        if inner_bag['bag_color'] == 'shiny gold':
+            count += 1
+        count += count_of_gold(inner_bag['bag_color'], rules)
     return count
 
+
+def count_of_inner_bags(rules, bag):
+    count = 0
+    inner_bags = rules[bag]
+    if not inner_bags:
+        return count
+    for inner_bag in inner_bags:
+        current_count = inner_bag['count']
+        count += current_count
+        nested_bag_count = count_of_inner_bags(rules, inner_bag['bag_color'])
+        count += (nested_bag_count * current_count)
+    return count
 
 def parse_rules(input_data):
     rules = defaultdict(list)
@@ -55,3 +73,4 @@ def load_data():
 
 if __name__ == '__main__':
     part1()
+    part2()
