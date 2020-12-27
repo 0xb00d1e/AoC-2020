a82f9cd2a71a7adbedef898ff605a79c1aa52a42
b00d1e <0xb00d1e@protonmail.com>
2020-12-26 19:36:15 -0500

    Day 6 - Part 2

--- 6/solution.py
+++ 6/solution.py
@@ -1,14 +1,31 @@
 def part1():
     input_data = load_data()
-    parsed_answers = parse_answers(input_data)
+    parsed_answers = parse_answers_1(input_data)
     answer = sum(len(sub) for sub in parsed_answers)
     print(f'Answer - Part 1: {answer}')
 
 
-def parse_answers(input_data):
+def part2():
+    input_data = load_data()
+    parsed_answers = parse_answers_2(input_data)
+    accumulator = 0
+    for answers in parsed_answers:
+        intersection = set.intersection(*answers)
+        accumulator += len(intersection)
+    print(f'Answer - Part 2: {accumulator}')
+
+
+def parse_answers_1(input_data):
     parsed = []
     for answers in input_data.split('\n\n'):
         parsed.append(set(''.join(answer for answer in answers.split('\n'))))
+    return parsed
+
+
+def parse_answers_2(input_data):
+    parsed = []
+    for answers in input_data.split('\n\n'):
+        parsed.append([set(answer) for answer in answers.split('\n') if answer])
     return parsed
 
 
@@ -20,3 +37,4 @@ def load_data():
 
 if __name__ == '__main__':
     part1()
+    part2()
